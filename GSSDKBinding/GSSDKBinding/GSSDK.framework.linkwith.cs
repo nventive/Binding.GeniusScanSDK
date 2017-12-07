5b2d18a69024321e2f7fd20bae34c4c045581b54
Benjamin Cartier <benjamin.cartier@nventive.com>
2017-12-07 15:38:24 -0500

    Added missing LinkWith property

---
+++ GSSDKBinding/GSSDKBinding/GSSDK.framework.linkwith.cs
@@ -0,0 +1,8 @@
+﻿using ObjCRuntime;
+
+[assembly: LinkWith("GSSDK.framework",
+	target: LinkTarget.Arm64 | LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator | LinkTarget.Simulator64,
+	SmartLink = true,
+	ForceLoad = true,
+	LinkerFlags = "-libstdc++ -ObjC",
+	Frameworks = "AVFoundation Foundation CoreGraphics CoreMedia UIKit")]
