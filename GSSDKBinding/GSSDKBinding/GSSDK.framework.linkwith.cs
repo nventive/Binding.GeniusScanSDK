5bfb9d690ddec61f73704c1e16617d50378b4c4a
Benjamin Cartier <benjamin.cartier@nventive.com>
2017-12-07 16:23:44 -0500

    Removed explicit targets

--- GSSDKBinding/GSSDKBinding/GSSDK.framework.linkwith.cs
+++ GSSDKBinding/GSSDKBinding/GSSDK.framework.linkwith.cs
@@ -1,8 +1,9 @@
 ﻿using ObjCRuntime;
 
-[assembly: LinkWith("GSSDK.framework",
-	target: LinkTarget.Arm64 | LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator | LinkTarget.Simulator64,
+[assembly: LinkWith(
+	"GSSDK.framework",
 	SmartLink = true,
 	ForceLoad = true,
 	LinkerFlags = "-libstdc++ -ObjC",
-	Frameworks = "AVFoundation Foundation CoreGraphics CoreMedia UIKit")]
+	Frameworks = "AVFoundation Foundation CoreGraphics CoreMedia UIKit"
+)]
